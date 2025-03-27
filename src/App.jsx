011f2be1d303af754c90a826c3fbb9df32542052
SiddiqueXTech <siddiq.slhri@gmail.com>
2025-03-27 07:32:36 +0500

    Created about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Home } from './Home'
 import { Shop } from './Shop'
+import { About } from './About'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/shop' element={<Shop/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
       </BrowserRouter>
     </>
